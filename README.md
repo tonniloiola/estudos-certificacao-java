bc1508c5d645863b31d6ac033f7a96f92a231ec9
tonniloiola <tonniloiola@gmail.com>
2018-04-30 14:11:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# estudos-certificacao-java" 
---
+++ Teste.java
@@ -0,0 +1,15 @@
+public class Teste {
+	
+	public void m1(){
+		int x = 10;
+		
+		System.out.println(x);
+	}
+	
+	public static void main(String[] args){
+		Teste t = new Teste();
+		t.m1();
+	}
+	
+	
+}
